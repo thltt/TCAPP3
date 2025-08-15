6b7fd236eb8ca9230484f178257442d5454853db
THLT <nhuhuynhnho254@gmail.com>
2025-08-16 05:55:11 +0700

    fix start

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-require("dotenv").config();
 const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, ".env") });
 
 const app = express();
 const port = process.env.PORT || 8050;
@@ -11,11 +11,6 @@ const port = process.env.PORT || 8050;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static(path.join(__dirname, "public"))); // thư mục chứa index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
 
 const pool = require("./config/database");
 
