ea3fab00dbc56c91d319da37269ef7ed5a0a34a0
THLT <nhuhuynhnho254@gmail.com>
2025-08-16 05:42:48 +0700

    fix index

--- src/server.js
+++ src/server.js
@@ -3,12 +3,19 @@ const mysql = require("mysql2");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 8050;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public"))); // thư mục chứa index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const pool = require("./config/database");
 
